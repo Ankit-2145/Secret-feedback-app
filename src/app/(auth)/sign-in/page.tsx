30fef8de14b86e64137d4fd68653653f132df5c2
Ankit-2145 <ankitsharma2615@gmail.com>
2024-08-27 12:08:11 +0530

    sign up done

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,37 +1,9 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import Link from "next/link";
-import { useState } from "react";
-import { useDebounceValue } from "usehooks-ts";
-import { useToast } from "@/components/ui/use-toast";
-import { useRouter } from "next/navigation";
-import { signUpSchema } from "@/schemas/signUpSchema";
+import React from 'react'
 
 const page = () => {
-  const [username, setusername] = useState("");
-  const [usernameMessage, setUsernameMessage] = useState("");
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  return (
+    <div>Sign in page</div>
+  )
+}
 
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const debouncedUsername = useDebounceValue(username, 300);
-  const { toast } = useToast();
-  const router = useRouter();
-
-  // zod implementation
-  const form = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema),
-    defaultValues: {
-      username: "",
-      email: "",
-      password: "",
-    },
-  });
-
-  return <div>page</div>;
-};
-
-export default page;
+export default page
